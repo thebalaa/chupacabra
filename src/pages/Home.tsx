dd87271dcd59909ff38d2cdeade850a1268a0cbb
Patrick De La Garza <patrickdlg@gmail.com>
2020-10-22 12:16:36 -0700

    Add Helmet + Logo + Sync Spinner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,33 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon} from '@ionic/react'
 import React from 'react'
+import {Helmet} from "react-helmet"
 import PostList from '../components/feed/PostList'
-import FeedFab from '../components/feed/FeedFab'
+import SyncLoader from '../components/feed/SyncLoader'
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Feed <IonIcon className="feed-icon" size="large" src="/assets/icon/feed.svg" /></IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+    <>
+      <Helmet>
+        <title> Feed | Chupacabra </title>
+      </Helmet>
+      <IonPage>
+        <IonHeader>
           <IonToolbar>
             <IonTitle>Feed <IonIcon className="feed-icon" size="large" src="/assets/icon/feed.svg" /></IonTitle>
           </IonToolbar>
         </IonHeader>
-        <PostList />
-        <FeedFab />
-      </IonContent>
-    </IonPage>
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle>Feed <IonIcon className="feed-icon" size="large" src="/assets/icon/feed.svg" /></IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <PostList />
+          <SyncLoader />
+        </IonContent>
+      </IonPage>
+    </>
   );
 };
 
