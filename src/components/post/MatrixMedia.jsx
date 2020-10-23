93676156e94ab62e9549c213a5ad8343e2d28672
Patrick De La Garza <patrickdlg@gmail.com>
2020-10-23 13:36:47 -0700

    Use  attribute to set Post content in html5, fallback to escaped source

--- src/components/post/MatrixMedia.jsx
+++ src/components/post/MatrixMedia.jsx
@@ -4,12 +4,14 @@ import {usePostFromUrl} from '../../recoil/feed'
 import {postMediaSelector} from '../../recoil/media'
 import './MatrixMedia.css'
 
-const MatrixMedia: React.FC = () => {
+const MatrixMedia = () => {
   const post = usePostFromUrl()
   const content = useRecoilValue(postMediaSelector(post.uri))
   return (
     <>
-      {content && <iframe title={post.title} className="media-frame" src={`data:text/html;charset=utf-8,${escape(content)}`}/>}
+      {content && <iframe title={post.title} className="media-frame"
+        src={`data:text/html;charset=utf-8,${escape(content)}`}
+        srcdoc={content}/>}
     </>
   )
 }
