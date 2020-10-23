30001c2ae9cdf2ef98bde0c180df08eeaa096d54
Patrick De La Garza <patrickdlg@gmail.com>
2020-10-23 11:36:23 -0700

    rendering posts in iframe

--- src/components/post/MatrixMedia.tsx
+++ src/components/post/MatrixMedia.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import {useRecoilValue} from 'recoil'
 import {usePostFromUrl} from '../../recoil/feed'
 import {postMediaSelector} from '../../recoil/media'
+import './MatrixMedia.css'
 
 const MatrixMedia: React.FC = () => {
   const post = usePostFromUrl()
   const content = useRecoilValue(postMediaSelector(post.uri))
   return (
     <>
-    {content && <div dangerouslySetInnerHTML={{__html: content}}/>}
+      {content && <iframe title={post.title} className="media-frame" src={`data:text/html;charset=utf-8,${escape(content)}`}/>}
     </>
   )
 }
